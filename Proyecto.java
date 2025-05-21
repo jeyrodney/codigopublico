a7b11f075029a8b1e56db0b7444e66259ebd5b50
jeyrodney <redes.ulloa@gmail.com>
2025-05-21 09:50:05 -0500

    Create Proyecto.java

---
+++ Proyecto.java
@@ -0,0 +1,7 @@
+public class Proyecto {
+    private String nombre;
+    private String descripcion;
+    private String nivelInnovacion;
+    private String estado;
+    private Emprendedor emprendedor
+}
